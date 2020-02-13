3d5dc9fe4b455de9f3ecabc7d7a6a3a32546ebe6
John Stempowski <johnstempowski@quickenloans.com>
2020-02-13 16:32:36 -0500

    Added a search feature

--- CarLot.cs
+++ CarLot.cs
@@ -13,19 +13,67 @@ namespace lab5_3
         }
         public void AddCar(Car c)
         {
+
+
             inventory.Add(c);
+
         }
 
         public void RemoveCar(int id)
         {
-            for (int i = 0;i < inventory.Count; i++)
+            for (int i = 0; i < inventory.Count; i++)
             {
                 if (inventory[i].Id == id)
                 {
                     inventory.Remove(inventory[i]);
                 }
-            }               
+            }
         }
+
+        public void SearchCar(string make)
+        {
+
+            foreach (Car c in inventory)
+            {
+
+                if (c.Make == make)
+                {
+                    Console.WriteLine(c);
+                }
+                else if (c.Model == make)
+                {
+                    Console.WriteLine(c);
+                }
+            }
+        }
+
+        public void SearchCar(int input)
+        {
+
+            foreach (Car c in inventory)
+            {
+
+                if (c.Year <= input)
+                {
+                    Console.WriteLine(c);
+                }
+
+            }
+        }
+        public void SearchCar(double input)
+        {
+
+            foreach (UsedCar c in inventory)
+            {
+
+                if (c.Mileage <= input)
+                {
+                    Console.WriteLine(c);
+                }
+
+            }
+        }
+
         public void ListCars()
         {
             foreach (Car c in inventory)
--- Program.cs
+++ Program.cs
@@ -6,42 +6,42 @@ namespace lab5_3
     class Program
 
     {
-        
-
-
-
         static List<Car> cars = new List<Car>();
         static void Main(string[] args)
         {
 
             int choice;
-          
+
             string make;
             string model;
             int year;
             int price;
             double mileage;
-            int id;
+            int id = 2;
             int buy;
+            string searchMake;
+
             CarLot lot1 = new CarLot();
             Console.WriteLine("Welcome to the Car Lot!");
-            lot1.AddCar(new UsedCar("Oldsmobile", "Intrigue", 1999, 5000, 1, 60000));
-            lot1.AddCar(new UsedCar("Ford", "Edge", 2017, 25000, 2, 40000));
-            MenuItems();
-            Console.WriteLine();
+            lot1.AddCar(new UsedCar("OLDSMOBILE", "INTRIGUE", 1999, 5000, 1, 60000));
+            lot1.AddCar(new UsedCar("FORD", "EDGE", 2017, 25000, 2, 40000));
+            
 
             do
             {
-                Console.Write("Please select an option");
+                Console.Clear();
+                MenuItems();
+                Console.WriteLine();
+                Console.Write("Please select an option 1-5: ");
                 choice = int.Parse(Console.ReadLine());
 
                 if (choice == 1)
                 {
                     Console.Write("Please provide the Make: ");
-                    make = Console.ReadLine();
+                    make = Console.ReadLine().ToUpper();
 
                     Console.Write("Please Provide the Model: ");
-                    model = Console.ReadLine();
+                    model = Console.ReadLine().ToUpper(); ;
 
                     Console.Write("Please enter the year: ");
                     year = int.Parse(Console.ReadLine());
@@ -49,14 +49,13 @@ namespace lab5_3
                     Console.Write("Please enter the price: ");
                     price = int.Parse(Console.ReadLine());
 
-                    Console.Write("Please enter the ID: ");
-                    id = int.Parse(Console.ReadLine());
+                    id++;
 
-                    Console.WriteLine("Please provide the mileage: ");
+                    Console.Write("Please provide the mileage: ");
                     mileage = double.Parse(Console.ReadLine());
                     lot1.AddCar(new UsedCar(make, model, year, price, id, mileage));
                 }
-                
+
                 if (choice == 2)
                 {
                     Console.Clear();
@@ -66,8 +65,6 @@ namespace lab5_3
                     buy = int.Parse(Console.ReadLine());
 
                     lot1.RemoveCar(buy);
-                    
-
                 }
 
                 if (choice == 3)
@@ -75,19 +72,60 @@ namespace lab5_3
                     lot1.ListCars();
                 }
 
+
                 if (choice == 4)
+                {
+                    Console.Clear();
+                    SearchOptions();
+                    Console.Write("Please enter a number 1-4: ");
+                    choice = int.Parse(Console.ReadLine());
+
+                    if (choice == 1)
+                    {
+                        Console.Write("Please Enter a Make to search by: ");
+                        searchMake = Console.ReadLine().ToUpper();
+                        lot1.SearchCar(searchMake);
+                    }
+                    else if (choice == 2)
+                    {
+                        Console.Write("Please Enter a Model to search by: ");
+                        string searchModel = Console.ReadLine().ToUpper();
+                        lot1.SearchCar(searchModel);
+                    }
+                    else if (choice == 3)
+                    {
+                        Console.Write("Please Enter a Year to search by: ");
+                        int searchYear = int.Parse(Console.ReadLine());
+                        lot1.SearchCar(searchYear);
+                    }
+                    else if (choice == 4)
+                    {
+                        Console.Write("Please Enter a Maximum number of miles to search by: ");
+                        double searchMiles = int.Parse(Console.ReadLine());
+                        lot1.SearchCar(searchMiles);
+                    }
+
+
+
+
+
+                }
+
+                if (choice == 5)
                 {
                     break;
                 }
-                
-                
+
+
+
+
             } while (GoAgain());
-            
-
-            
 
 
-            
+
+
+
+
         }
 
         static bool GoAgain()
@@ -109,11 +147,25 @@ namespace lab5_3
 
         static void MenuItems()
         {
+            Console.WriteLine("=================================");
             Console.WriteLine("1. Add a Car");
             Console.WriteLine("2. Buy a Car");
             Console.WriteLine("3. Print List of Inventory");
-            Console.WriteLine("4. Quit");
+            Console.WriteLine("4. Search by Make");
+            Console.WriteLine("5. Quit");
+            Console.WriteLine("=================================");
         }
-        
+
+        static void SearchOptions()
+        {
+            Console.WriteLine("=================================");
+            Console.WriteLine("What would you like to search by? ");
+            Console.WriteLine("1. Make");
+            Console.WriteLine("2. Model");
+            Console.WriteLine("3. Year");
+            Console.WriteLine("4. Price");
+            Console.WriteLine("=================================");
+
+        }
     }
 }
